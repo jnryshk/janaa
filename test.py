4724d38c9068614317ffaa48eeca869a615c9d73
jnryshk <janula.20062007@gmail.com>
2023-03-23 12:14:36 +0100

    fg

--- test.py
+++ test.py
@@ -1,30 +1,59 @@
-figura=input("podaj jedną z podanych figur :prostokąt, trójkąt, równoległobok, romb, trapez, kolo ")
+import math
+
+figura=input("podaj jedną z podanych figur :prostokąt, trójkąt, równoległobok, romb, trapez, koło: ")
 if figura == "prostokąt":
-    a=float(input("podaj długość pierwszego boku "))
-    b=float(input("podaj długość drugiego boku "))
+    a=float(input("podaj długość pierwszego boku:  "))
+    b=float(input("podaj długość drugiego boku: "))
+    while a<=0 or b<=0:
+         print("podaj liczby dodatnie")
+         a=float(input("podaj długość pierwszego boku:  "))
+         b=float(input("podaj długość drugiego boku: "))
     pole=a*b
-    print("pole twojego prostokątu to:" , pole )
+    print("pole twojego prostokątu to: " , pole )
+
 elif figura=="trójkąt":
-    a=float(input("podaj długość podstawy trojkątu "))
-    h=float(input("podaj wysokość trójkątu "))
+    a=float(input("podaj długość podstawy trojkątu: "))
+    h=float(input("podaj wysokość trójkątu: "))
+    while a<=0 or h<=0:
+        print("podaj wartości dodatnie")
+        a=float(input("podaj długość podstawy trojkątu: "))
+        h=float(input("podaj wysokość trójkątu: "))
     pole1=0.5*a*h
-    print("pole twojego trójkątu to:" , pole1)
+    print("pole twojego trójkątu to: " , pole1)
+
 elif figura=="równoległobok":
-    a=float(input("podaj dlugość boku "))
-    h=float(input("podaj wysokość "))
+    a=float(input("podaj dlugość boku: "))
+    h=float(input("podaj wysokość: "))
+    while a<=0 or h<=0:
+        print("podaj liczby dodatnie")
+        a=float(input("podaj dlugość boku: "))
+        h=float(input("podaj wysokość: "))
     pole2=a*h
-    print("pole twojego trójkątu to:" , pole2)
+    print("pole twojego trójkątu to: " , pole2)
 elif figura=="romb":
-    e=float(input("podaj dlugość pierwszej przekątnej "))
-    f=float(input("podaj dlugość pierwszej przekątne "))
+    e=float(input("podaj dlugość pierwszej przekątnej:  "))
+    f=float(input("podaj dlugość pierwszej przekątnej:  "))
+    while e<=0 or f<=0:
+         print("podaj liczby dodatnie")
+         e=float(input("podaj dlugość pierwszej przekątnej:  "))
+         f=float(input("podaj dlugość pierwszej przekątnej:  "))
     pole3=0.5*e*f
-    print("pole twojego trójkątu to:" , pole3)
+    print("pole twojego trójkątu to: " , pole3)
+   
 elif figura=="trapez":
-    a=float(input("podaj dlugość górnego boku trapezu "))
-    b=float(input("podaj dlugość dolnego boku trapezu "))
-    h=float(input("podaj wysokość trapezu "))
+    a=float(input("podaj dlugość górnego boku trapezu: "))
+    b=float(input("podaj dlugość dolnego boku trapezu: "))
+    h=float(input("podaj wysokość trapezu: "))
     c=a+b
-    pole4=0.5*c*h
-    print("pole twojego trapezu to:" , pole4)
+    if a>0 and b>0 and h>0:
+        pole4=0.5*c*h
+        print("pole twojego trapezu to: " , pole4)
+    else :
+        print("podaj liczby dodatnie")
 elif figura=="koło":
-    r=float(input("podaj dlugość górnego boku trapezu "))
+    r=float(input("podaj promień koła: "))
+    if r>0 :
+        pole5=r**2*math.pi
+        print("pole twojego kołą to: " , pole5) 
+    else :
+        print("podaj liczby dodatnie")
