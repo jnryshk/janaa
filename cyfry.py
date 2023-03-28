efa1ce7209967f40bd8c79899442406e7f793bbb
jnryshk <janula.20062007@gmail.com>
2023-03-28 12:11:13 +0200

    1

---
+++ cyfry.py
@@ -0,0 +1,9 @@
+a=input("podaj liczbę: ")
+if a=="1":
+    print("rzymska liczba : i ")
+elif a=="2":
+     print("rzymska liczba : ii ")
+elif a=="3":
+     print("rzymska liczba : iii ")
+elif a=="4":
+     print("rzymska liczba : iv ")
